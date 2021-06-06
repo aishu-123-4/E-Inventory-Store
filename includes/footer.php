0f280b7c9e80f775ff91a2fa9a64745323463e3d
AISWARYA SATISH <67549464+aishu-123-4@users.noreply.github.com>
2021-06-06 17:01:51 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+	</div>
+	<!-- /container -->
+
+	<!-- file input -->
+	<script type="text/javascript" src="assets/plugins/fileinput/js/plugins/canvas-to-blob.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/fileinput/js/plugins/sortable.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/fileinput/js/plugins/purify.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/fileinput/js/fileinput.min.js"></script>
+	<!-- datatables js -->
+	<script type="text/javascript" src="assets/plugins/datatables/datatables.min.js"></script>
+
+
+<div class="footer-copyright text-center">
+        © 2021
+        <a href="#">E-Inventory store by Group 4</a>
+    </div>
+
+</body>
+</html>
