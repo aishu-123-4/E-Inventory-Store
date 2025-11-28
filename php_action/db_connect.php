d09d3c1ba6f0c675556c9f020639c3e78bf12ad3
AISWARYA SATISH <67549464+aishu-123-4@users.noreply.github.com>
2025-11-28 23:03:12 +0530

    Update db_connect.php

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -2,7 +2,7 @@
 
 $localhost = "localhost";
 $username = "root";
-$password = "root";
+$password = "***";
 $dbname = "inventory";
 
 $connect = new mysqli($localhost, $username, $password, $dbname);
